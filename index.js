c6a6bc616eb5cc570ca7272471f7d371329dcd44
jotazz25 <jalvarez@tecbaco.com>
2023-06-02 16:41:15 -0500

    api con node

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("my app")
+const express = require('express');
+const app = express()
+const port = 5000
+const fruits = require('./fruits.json')
+
+app.get('/', (req,res) => {
+  res.send('Bienvenido a mi servidor en express');
+})
+
+app.get('/nuevaruta', (req, res) => {
+  res.send("Nueva ruta")
+})
+
+app.get('/fruits', (req, res) => {
+  res.json(fruits)
+})
+
+app.get('/fruit/:id', (req,res) => {
+  const {id} = req.params
+  const fruit = fruits.find(
+    (d) => d.id == id
+  )
+  res.json(fruit)
+})
+
+app.listen(port, () => {
+  console.log('Mi port: ' + port)
+})
